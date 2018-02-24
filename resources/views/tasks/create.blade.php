b81799ec4349cb3968eaaf2e7230b316f6229bb8
Tatsuo Asahi <ec2-user@ip-172-31-27-6.us-east-2.compute.internal>
2018-02-24 10:05:51 +0000

    Procfile

--- resources/views/tasks/create.blade.php
+++ resources/views/tasks/create.blade.php
@@ -3,10 +3,11 @@
 @section('content')
 
  <h1>タスク新規作成ページ</h1>
-
+ 
     {!! Form::model($task, ['route' => 'tasks.store']) !!}
 <div class="row">
- <div class="col-xs-12 col-sm-8 col-md-8 .col-lg-6">
+ <div class="col-xs-12 col-sm-offset-2 col-sm-8 col-sm-offset-2 col-md-8 col-lg-offset-3 col-lg-6">
+ 
 
         {!! Form::label('title', 'タスク:') !!}
         {!! Form::text('title', null, ['class' => 'form-control']) !!}
@@ -22,6 +23,8 @@
     
      </div>
     </div>
+    </div>
+    
 
 
 @endsection
