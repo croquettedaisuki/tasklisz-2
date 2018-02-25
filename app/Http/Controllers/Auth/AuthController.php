17947c07acc4b63713306258b1d227112b0460d3
Tatsuo Asahi <ec2-user@ip-172-31-27-6.us-east-2.compute.internal>
2018-02-25 11:38:41 +0000

    login

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -22,6 +22,10 @@ class AuthController extends Controller
     */
 
     use AuthenticatesAndRegistersUsers, ThrottlesLogins;
+    
+    //課題提出のため追加
+    protected $redirectTo = '/';
+    protected $loginPath = '/login';
 
     /**
      * Create a new authentication controller instance.
