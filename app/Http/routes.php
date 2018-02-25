8b6201c7ee574a9a92f9239cac6cbc988b7b173e
Tatsuo Asahi <ec2-user@ip-172-31-27-6.us-east-2.compute.internal>
2018-02-25 13:58:42 +0000

    Revert "routes-welcomeblade-change" matigaetanodesakujo
    This reverts commit 63fc0d2cb6da07a385c3a9b207f32ae8524f66f2.

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -49,9 +49,3 @@ Route::resource('tasks', 'TasksController');
 Route::get('login', 'Auth\AuthController@getLogin')->name('login.get');
 Route::post('login', 'Auth\AuthController@postLogin')->name('login.post');
 Route::get('logout', 'Auth\AuthController@getLogout')->name('logout.get');
-
-
-//ログイン認証
-Route::group(['middleware' => 'auth'], function () {
-    Route::resource('users', 'UsersController', ['only' => ['index', 'show']]);
-});
